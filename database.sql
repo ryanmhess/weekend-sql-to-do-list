1fc03bc6ec9c1b2b5467928a0c21403815c2129a
Ryan Hess <ryanmhess@gmail.com>
2022-09-11 19:02:25 -0500

    updated database.sql

--- database.sql
+++ database.sql
@@ -4,8 +4,8 @@ CREATE TABLE "toDoList"
 	(
 	"id" SERIAL PRIMARY KEY,
 	"priority" VARCHAR(25) NOT NULL,
-	"owner" VARCHAR(50) NOT NULL,
-	"task" VARCHAR (25) NOT NULL,
+	"adventurer" VARCHAR(50) NOT NULL,
+	"location" VARCHAR (25) NOT NULL,
     "details" VARCHAR(250) NOT NULL,
     "start" VARCHAR,
     "finish" VARCHAR, 
@@ -13,11 +13,13 @@ CREATE TABLE "toDoList"
     );
     
 -- test data
-INSERT INTO "toDoList" (priority, owner, task, details, start, complete)
+INSERT INTO "toDoList" (priority, adventurer, location, details, start, finish, complete)
 	VALUES
-		('high', 'Ryan', 'Kitchen', 'Clean out stove and counter tops', '12:20 pm', false),
-		('medium', 'Julia', 'Bathroom', 'Wipe down toilet and shower', '1:34 pm', true),
-		('low', 'Emily', 'Pets', 'Kitty litter', '9:34 am', false),
-		('high', 'Ian', 'Bedroom', 'Pick up toys and make bed', '11:34 am', false),
-		('medium', 'Kellen', 'Bedroom', 'Put away clean clothes and bring down dirty clothes', '10:34 am', false),
-		('low', 'Ryan', 'Garage', 'Prep garage for winter', '7:54 pm', true);
+		('HIGH', 'Ryan', 'Kitchen', 'Clean out stove and counter tops', '12:20 pm', null, false),
+		('MEDIUM', 'Julia', 'Bathroom', 'Wipe down toilet and shower', '1:34 pm', '2:16 pm', true),
+		('LOW', 'Emily', 'Pets', 'Kitty litter', '9:34 am', null, false),
+		('HIGH', 'Ian', 'Bedroom', 'Pick up toys and make bed', '11:34 am', null, false),
+		('MEDIUM', 'Kellen', 'Bedroom', 'Put away clean clothes and bring down dirty clothes', '10:34 am', null, false),
+		('LOW', 'Ryan', 'Garage', 'Prep garage for winter', '7:54 pm', '853 pm', true),
+		('HIGH', 'Kellen', 'Self', 'Cut my snaggle toothed toenails', '11:00 am', null, false),
+		('MEDIUM', 'Emily', 'Bedroom', 'Put away your clothes', '8:14 am', null, false);
